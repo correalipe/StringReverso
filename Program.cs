a1450994f3efbef891de190648b70922080a5ac5
correalipe <111357309+correalipe@users.noreply.github.com>
2023-03-23 19:59:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //variables
+        string word, revWord = "";
+
+        //input
+        Console.WriteLine("Escreva uma palavra: ");
+        word = Console.ReadLine();
+
+        //loop & check
+        for (int i  = word.Length-1; i>=0; i--)
+        {
+            revWord = revWord + word[i];
+        }
+
+        //output
+        Console.WriteLine("Palavra de trás para frente é {0}", revWord);
+
+        //palindromo
+        if (word == revWord)
+        {
+            Console.WriteLine("A palavra é palíndromo");
+        }
+    }
+}
